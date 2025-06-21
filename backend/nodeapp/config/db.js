a3c7209981d2368568193c5337d50a161aed45d1
Ly Phan Thị Khánh <khanhlypt14@gmail.com>
2025-06-21 23:50:13 +0700

    d

--- backend/nodeapp/config/db.js
+++ backend/nodeapp/config/db.js
@@ -1,14 +1,9 @@
 import mongoose from 'mongoose'
-import Bike from '../models/Bike';
 
 const connectDB = async () => {
     try {
         console.log(process.env.MONGO_URI)
         await mongoose.connect(process.env.MONGO_URI);
-        await Bike.updateMany({}, {
-        $unset: { price: "" },
-        $set: { price: 100000 }
-        }); 
         console.log("Mongo connected successfully");
     } catch(err) {
         console.error('Mongo connected failed:', err.message);
